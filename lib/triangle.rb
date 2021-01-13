444a322360a8176b12e483031b21c1c5b2cd6dcf
Danny Brandt <dvbrandt90@gmail.com>
2021-01-13 18:31:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,9 @@ class Triangle
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       begin
         raise TriangleError 
-      elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side3 + @side1 <= @side2
+      end
+    end
+    if @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side3 + @side1 <= @side2
       begin
         raise TriangleError 
       end
